45462029dc009658345057a0b94fb2066ba68369
zhengxw <zhengxiaowei01@baidu.com>
2016-05-30 18:28:25 +0800

    修改部分文件的文件夹位置

---
+++ app/src/main/java/com/kay/duitang/ui/fragment/MoveFragment.java
@@ -0,0 +1,66 @@
+package com.kay.duitang.ui.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.etsy.android.grid.StaggeredGridView;
+import com.kay.duitang.R;
+import com.kay.duitang.adapter.StaggerItemAdapter;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by t2porn on 16/5/30
+ */
+public class MoveFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
+
+    @InjectView(R.id.stagger_view)
+    StaggeredGridView mStaggeredGridView;
+
+    @InjectView(R.id.refresh_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private static Handler handler;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_stagger, container, false);
+        ButterKnife.inject(this, view);
+        handler = new Handler();
+        initView();
+        return view;
+    }
+
+    private void initView() {
+        mStaggeredGridView.setAdapter(new StaggerItemAdapter(getActivity(), true));
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.blue);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+
+    @Override
+    public void onRefresh() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(getActivity(), getString(R.string.refresh_success), Toast.LENGTH_SHORT).show();
+            }
+        }, 3000);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        handler.removeCallbacksAndMessages(null);
+    }
+}
---
+++ app/src/main/java/com/kay/duitang/ui/fragment/MsgFragment.java
@@ -0,0 +1,23 @@
+package com.kay.duitang.ui.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kay.duitang.R;
+
+/**
+ * Created by Kay on 15/4/23.
+ */
+public class MsgFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_msg, container, false);
+        return view;
+    }
+}
