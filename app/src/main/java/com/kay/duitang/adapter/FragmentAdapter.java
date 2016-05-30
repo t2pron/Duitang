97cf1b2150a6dc73147f5d675107cdab96b20ff0
zhengxw <zhengxiaowei01@baidu.com>
2016-05-30 18:28:02 +0800

    规范部分代码。 修改样式

--- app/src/main/java/com/kay/duitang/adapter/FragmentAdapter.java
+++ app/src/main/java/com/kay/duitang/adapter/FragmentAdapter.java
@@ -4,11 +4,11 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v13.app.FragmentStatePagerAdapter;
 
-import com.kay.duitang.ui.DiscoverFragment;
-import com.kay.duitang.ui.MsgFragment;
-import com.kay.duitang.ui.PagerFragment;
-import com.kay.duitang.ui.PopularFragment;
-import com.kay.duitang.ui.MoveFragment;
+import com.kay.duitang.ui.fragment.DiscoverFragment;
+import com.kay.duitang.ui.fragment.MsgFragment;
+import com.kay.duitang.ui.fragment.PagerFragment;
+import com.kay.duitang.ui.fragment.PopularFragment;
+import com.kay.duitang.ui.fragment.MoveFragment;
 
 /**
  * Created by Kay on 15/4/11.
@@ -19,19 +19,21 @@ public class FragmentAdapter extends FragmentStatePagerAdapter {
         super(fm);
     }
 
-    private String[] pageTitle = {"热门", "发现", "动态", "消息"};
+    private String[] pageTitle = {"热门",  "发现","设置"}; //发现 , "消息"
 
     @Override
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                return new PopularFragment();
+                return new MoveFragment();
             case 1:
                 return new DiscoverFragment();
             case 2:
-                return new MoveFragment();
-            case 3:
                 return new MsgFragment();
+          /*  case 2:
+                return new PopularFragment();
+            case 3:
+                return new MsgFragment();*/
             default:
                 return PagerFragment.newInstance(position + "");
         }
